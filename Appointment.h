9e990f878bf615b32db734639b53df731e90149c
taishicoding <a1904976@adelaide.edu.au>
2024-05-20 14:40:25 +0930

    Fifth Upload

--- Appointment.h
+++ Appointment.h
@@ -1,25 +1,30 @@
 #ifndef APPOINTMENT_H
 #define APPOINTMENT_H
 
-#include "Patient.h"
-#include "Hospital.h"
-
+#include <string>
 
 class Appointment {
+private:
+    std::string appointmentID;
+    std::string date;
+    std::string time; 
+    std::string patientID;
+    std::string doctorID; 
+    std::string description; 
 
-protected: 
-
-    int appointmentID; 
-
-public: 
-
-    Appointment(); 
-
-    int getAppointmentID(); 
-
+public:
+    Appointment(const std::string& id, const std::string& date, const std::string& time,
+                const std::string& patientID, const std::string& doctorID, const std::string& description);
+    std::string getAppointmentID() const;
+    std::string getDate() const;
+    std::string getTime() const;
+    std::string getPatientID() const;
+    std::string getDoctorID() const;
+    std::string getDescription() const;
+    void setDate(const std::string& newDate);
+    void setTime(const std::string& newTime);
+    void setDescription(const std::string& newDescription);
+    void printInfo() const;
 };
 
-
-
-
-#endif
+#endif 
