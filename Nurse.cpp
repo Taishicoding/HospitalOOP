111b1ca18527a77667e1e57c569a50b81192fbfc
Taishicoding <163483782+Taishicoding@users.noreply.github.com>
2024-05-23 00:10:57 +0930

    Update Nurse.cpp

--- Nurse.cpp
+++ Nurse.cpp
@@ -1,8 +1,12 @@
 #include "Nurse.h"
 #include <iostream>
 
+// default constructor
+// sets up nurse with default values
 Nurse::Nurse() : MedStaff(), shiftLength(5), areaOfField("Health") {}
 
+// parameterised constructor
+// sets up nurse with given details and calls parent class constructor
 Nurse::Nurse(const std::string& name, int age, const std::string& contactNo,
              const std::string& employeeID, const std::string& department,
              const std::string& position, const std::string& qualifications,
@@ -10,31 +14,42 @@ Nurse::Nurse(const std::string& name, int age, const std::string& contactNo,
     : MedStaff(name, age, contactNo, employeeID, department, position, qualifications, experienceYears),
       shiftLength(shiftLength), areaOfField(areaOfField) {}
 
+// destructor
+// just a destructor for nurse class, doesn't do much
 Nurse::~Nurse() {}
 
+// gets the nurse's shift length
 int Nurse::getShiftLength() const {
     return shiftLength;
 }
 
+// gets the nurse's roles
 std::vector<std::string> Nurse::getRoles() const {
     return roles;
 }
 
+// gets the nurse's area of field
 std::string Nurse::getAreaOfField() const {
     return areaOfField;
 }
 
+// sets the nurse's shift length
 void Nurse::setShiftLength(int shiftLength) {
     this->shiftLength = shiftLength;
 }
 
+// adds a role to the nurse's list
 void Nurse::addRole(const std::string& role) {
     roles.push_back(role);
 }
 
+// sets the nurse's area of field
 void Nurse::setAreaOfField(const std::string& field) {
     this->areaOfField = field;
 }
+
+// prints out the nurse's info
+// calls parent class printInfo and then adds nurse details
 void Nurse::printInfo() const {
     MedStaff::printInfo(); 
     std::cout << "Shift Length: " << shiftLength << " hours\n";
@@ -45,4 +60,3 @@ void Nurse::printInfo() const {
     }
     std::cout << std::endl;
 }
-
