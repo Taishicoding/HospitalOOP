4b23ef23497a5f057ba52c83bcda1e1c85fa041a
Taishicoding <163483782+Taishicoding@users.noreply.github.com>
2024-05-23 00:16:40 +0930

    Update MedStaff.cpp

--- MedStaff.cpp
+++ MedStaff.cpp
@@ -1,48 +1,69 @@
 #include "MedStaff.h"
-#include "MedStaff.h"
 #include <iostream>
-//Apparently to use std::remove alogortihtm is needed smh
+// apparently to use std::remove algorithm is needed smh
 #include <algorithm>
 
-MedStaff::MedStaff() : Employee (), qualifications("cool"),experienceYears(9000){};
+// default constructor
+// sets up medstaff with default values
+MedStaff::MedStaff() : Employee(), qualifications("cool"), experienceYears(9000) {}
+
+// parameterised constructor
+// sets up medstaff with given details and calls parent class constructor
 MedStaff::MedStaff(const std::string& name, int age, const std::string& contactNo,
                    const std::string& employeeID, const std::string& department,
                    const std::string& position, const std::string& qualifications,
                    int experienceYears)
     : Employee(name, age, contactNo, employeeID, department, position),
-      qualifications(qualifications), experienceYears(experienceYears){}
-MedStaff::~MedStaff(){};
+      qualifications(qualifications), experienceYears(experienceYears) {}
 
-std::string MedStaff::getQualifications() const{
+// destructor
+// just a destructor for medstaff class, doesn't do much
+MedStaff::~MedStaff() {}
+
+// gets the medstaff's qualifications
+std::string MedStaff::getQualifications() const {
     return qualifications;
-};
+}
+
+// gets the medstaff's years of experience
 int MedStaff::getExperienceYears() const {
     return experienceYears;
-};
-std::vector<std::string> MedStaff::getAssignedpatients() const{
+}
+
+// gets the list of patients assigned to the medstaff
+std::vector<std::string> MedStaff::getAssignedpatients() const {
     return assignedPatients;
 }
-void MedStaff::setQualifications(const std::string& qualifications){
-    this-> qualifications = qualifications;
+
+// sets the medstaff's qualifications
+void MedStaff::setQualifications(const std::string& qualifications) {
+    this->qualifications = qualifications;
 }
-void MedStaff::setExperienceyears(const int experienceYears){
-    this-> experienceYears = experienceYears;
+
+// sets the medstaff's years of experience
+void MedStaff::setExperienceyears(const int experienceYears) {
+    this->experienceYears = experienceYears;
 }
-void MedStaff::setAssignedpatients(const std::string& patientID){
+
+// adds a patient to the medstaff's assigned list
+void MedStaff::setAssignedpatients(const std::string& patientID) {
     assignedPatients.push_back(patientID);
 }
-void MedStaff::unsetAssignedpatients(const std::string& patientID){
-    assignedPatients.erase(std::remove(assignedPatients.begin(),assignedPatients.end(),patientID),assignedPatients.end());
+
+// removes a patient from the medstaff's assigned list
+void MedStaff::unsetAssignedpatients(const std::string& patientID) {
+    assignedPatients.erase(std::remove(assignedPatients.begin(), assignedPatients.end(), patientID), assignedPatients.end());
 }
 
-void MedStaff::printInfo()const{
+// prints out the medstaff's info
+// calls parent class printInfo and then adds medstaff details
+void MedStaff::printInfo() const {
     Employee::printInfo();
-    std::cout<<"Qualifications:"<<" "<<std::endl;
-    std::cout<<"Years of Experience:"<<" "<<std::endl;
-    std::cout<<"Assigned Patients:";
-    for (const auto&patient :assignedPatients){
-        std::cout << patient << ",";
+    std::cout << "Qualifications: " << qualifications << std::endl;
+    std::cout << "Years of Experience: " << experienceYears << std::endl;
+    std::cout << "Assigned Patients: ";
+    for (const auto& patient : assignedPatients) {
+        std::cout << patient << ", ";
     }
-    std::cout<<std::endl;
-
+    std::cout << std::endl;
 }
