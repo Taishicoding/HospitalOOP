84e606c74b392cf6634e5a1225bf8c6d42f3a615
Taishicoding <163483782+Taishicoding@users.noreply.github.com>
2024-05-23 00:19:36 +0930

    Update Hospital.h

--- Hospital.h
+++ Hospital.h
@@ -10,37 +10,38 @@
 #include "Nurse.h"
 #include "Doctor.h"
 
+// hospital class definition
 class Hospital {
 private:
-    Patient** patients;
-    Appointment** appointments;
-    Employee** employees;
-    int currentPatients;
-    int currentAppointments;
-    int currentEmployees;
-    int patientCapacity;
-    int staffCapacity;
+    Patient** patients; // array of patients
+    Appointment** appointments; // array of appointments
+    Employee** employees; // array of employees
+    int currentPatients; // current number of patients
+    int currentAppointments; // current number of appointments
+    int currentEmployees; // current number of employees
+    int patientCapacity; // maximum number of patients
+    int staffCapacity; // maximum number of staff
 
 public:
-    Hospital();
-    Hospital(int numPatients, int numStaff);
-    ~Hospital();
+    Hospital(); // default constructor
+    Hospital(int numPatients, int numStaff); // constructor with parameters
+    ~Hospital(); // destructor
 
-    Patient** getPatientArray();
-    Appointment** getAppointmentArray();
-    Employee** getEmployeesArray();
-    int getCurrentPatients() const;
-    int getCurrentEmployees() const;
-    int getCurrentNumAppointments() const; 
+    Patient** getPatientArray(); // gets array of patients
+    Appointment** getAppointmentArray(); // gets array of appointments
+    Employee** getEmployeesArray(); // gets array of employees
+    int getCurrentPatients() const; // gets current number of patients
+    int getCurrentEmployees() const; // gets current number of employees
+    int getCurrentNumAppointments() const; // gets current number of appointments
 
-    void addNewPatient(std::string name, int age, std::string contact, std::string ID);
-    void addNewEmployee(std::string name, int age, std::string contactNo, std::string ID, std::string department, std::string position);
-    void addNewMedStaff(std::string name, int age, std::string contactNo, std::string ID, std::string department, std::string position, std::string qualifications, int experienceYears);
-    void addNewNurse(std::string name, int age, std::string contactNo, std::string employeeID, std::string department, std::string position, std::string qualifications, int experienceYears, int shiftLength, std::string areaOfField, std::string ID);
-    void addNewDoctor(std::string name, int age, std::string contactNo, std::string employeeID, std::string department, std::string position, std::string qualifications, int experienceYears, std::string specialisation);
+    void addNewPatient(std::string name, int age, std::string contact, std::string ID); // adds a new patient
+    void addNewEmployee(std::string name, int age, std::string contactNo, std::string ID, std::string department, std::string position); // adds a new employee
+    void addNewMedStaff(std::string name, int age, std::string contactNo, std::string ID, std::string department, std::string position, std::string qualifications, int experienceYears); // adds a new medical staff member
+    void addNewNurse(std::string name, int age, std::string contactNo, std::string employeeID, std::string department, std::string position, std::string qualifications, int experienceYears, int shiftLength, std::string areaOfField, std::string ID); // adds a new nurse
+    void addNewDoctor(std::string name, int age, std::string contactNo, std::string employeeID, std::string department, std::string position, std::string qualifications, int experienceYears, std::string specialisation); // adds a new doctor
 
-    void addAppointmentRequest(Appointment* appointment);
-    void approveAppointment(Appointment* appointment, double price);
+    void addAppointmentRequest(Appointment* appointment); // adds a new appointment request
+    void approveAppointment(Appointment* appointment, double price); // approves an appointment
 };
 
 #endif
