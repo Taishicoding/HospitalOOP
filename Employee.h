f4091f8b3d8242123cd471c6406714b444ebecd4
Taishicoding <163483782+Taishicoding@users.noreply.github.com>
2024-05-23 00:00:21 +0930

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,26 +1,27 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
+
 #include "Person.h"
 #include <string>
 
-class Employee: public Person{
-    protected:
-        std::string employeeID;
-        std::string department;
-        std::string position;
-    public:
-        Employee();
-        Employee(const std::string& name, int age,const std::string& contactNo, const std::string& employeeID, const std::string& department, const std::string& position);
-        virtual ~Employee();
-        std::string getEmployeeID() const;
-        std::string getDepartment() const;
-        std::string getPosition() const;
-        void setEmployeeID(const std::string& employeeID);
-        void setDepartment(const std::string& department);
-        void setPosition(const std::string& position);
-        virtual void printInfo() const override;
+// employee class inherits from person
+class Employee: public Person {
+protected:
+    std::string employeeID; // employee's id
+    std::string department; // employee's department
+    std::string position;   // employee's position
+
+public:
+    Employee(); // default constructor
+    Employee(const std::string& name, int age, const std::string& contactNo, const std::string& employeeID, const std::string& department, const std::string& position); // constructor with parameters
+    virtual ~Employee(); // destructor
+    std::string getEmployeeID() const; // gets employee id
+    std::string getDepartment() const; // gets department
+    std::string getPosition() const; // gets position
+    void setEmployeeID(const std::string& employeeID); // sets employee id
+    void setDepartment(const std::string& department); // sets department
+    void setPosition(const std::string& position); // sets position
+    virtual void printInfo() const override; // prints info about employee
 };
-
-
 
 #endif
